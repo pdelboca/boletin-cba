e2c662d63b32eaf57b9b8897eff2c95ed9c7c8aa
pdelboca <patriciodelboca@gmail.com>
2016-02-05 10:59:17 -0300

    pdf_to_csv method

---
+++ main.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  2 18:01:54 2016
+
+@author: pdelboca
+
+References:
+===========
+ - PyPDF2: https://github.com/mstamy2/PyPDF2
+
+"""
+import os.path
+from PyPDF2 import PdfFileReader
+import csv
+
+_DATA_PATH = os.path.realpath(os.path.dirname(__file__)) + "/data/"
+
+
+def pdf_to_csv():
+    pdf_data_path = _DATA_PATH + "pdf/"   
+    csv_data_file = _DATA_PATH + "data.csv"
+    with open(csv_data_file, "w", newline='') as csvfile:        
+        data_writer = csv.writer(csvfile)
+        data_writer.writerow(["id","texto"])        
+        for fn in os.listdir(pdf_data_path):
+            file_path = os.path.join(pdf_data_path, fn)         
+            if file_path.endswith(".pdf"):
+                input_file = PdfFileReader(open(file_path, 'rb'))
+                text = ""
+                for p in range(input_file.getNumPages()):
+                    text += input_file.getPage(p).extractText() + " "
+                data_writer.writerow([fn,text])
+        
+
+if __name__ == "__main__":
+    pdf_to_csv()
